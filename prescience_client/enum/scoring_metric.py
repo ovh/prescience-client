9ff34edad868d261ad14e0742f58effec44b4c2e
clement.bataille <clement.bataille@corp.ovh.com>
2019-06-11 11:51:27 +0200

    Adding MAPE metrics

--- prescience_client/enum/scoring_metric.py
+++ prescience_client/enum/scoring_metric.py
@@ -7,9 +7,28 @@ from enum import Enum, unique
 
 @unique
 class ScoringMetric(Enum):
+
+    # Classificaton
     ACCURACY = 'accuracy'
-    MSE = 'mse'
+    COHEN_KAPPA = 'cohen_kappa'
+    # Binary
+    F1 = 'f1'
+    ROC_AUC = 'roc_auc',
+    PRECISION = 'precision'
+    RECALL = 'recall'
     LOG_LOSS = 'log_loss'
+    # Multiclass
+    F1_MICRO = 'f1_micro'
+    F1_MACRO = 'f1_macro'
+    ROC_AUC_MICRO = 'roc_auc_micro'
+    ROC_AUC_MACRO = 'roc_auc_macro'
+    AVERAGE_PRECISION_MICRO = 'average_precision_micro'
+    AVERAGE_PRECISION_MACRO = 'average_precision_macro'
+    # Regression
+    MAE = 'mae'
+    MSE = 'mse'
+    MAPE = 'mape'
+    R2 = 'r2'
 
     def __str__(self):
         return self.value
